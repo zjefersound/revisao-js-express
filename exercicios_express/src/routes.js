2bcd454516e688c8fde8aa86ba293ea45c118079
zjefersound <jeferson.2017317264@gmail.com>
2020-05-06 00:46:33 -0300

    Fim revisão js e express

--- exercicios_express/src/routes.js
+++ exercicios_express/src/routes.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const body_parser = require('body-parser')
+const api_usuario = require('./controllers/UserController')
+const api_produto = require('./controllers/ProductController')
 //usa o router do express
 const routes = express.Router()
 //importando função middleware
 const saudacao = require('./controllers/saudacao_mid')
+
+api_produto(routes,'com param')
+
+/** Funções middleware para interpretar varios tipos de dados */
+routes.use(body_parser.text())
+routes.use(body_parser.json())
+routes.use(body_parser.urlencoded({ extended: true }))
+
 
 //Aqui vão as requisições
 routes.use(saudacao('Jeff'))
@@ -15,13 +26,16 @@ routes.use((req,res,next) => {
     next()/** Sem esse next a proxima função não executa */
 })
 
+routes.post('/usuario',api_usuario.salvar)
+routes.get('/usuario',api_usuario.obter)
+
 //Isso aqui é um query param
 routes.get('/clientes/relatorio',(req,res) => {//essa func é mais especifica
     const completo = req.query.completo
     res.send(`O param completo é:${completo}`)
 })
 
-//Isso é um body request
+//Isso é um body request sem body parser
 routes.post('/corpo',(req,res) => {
     let corpo = ''
     req.on('data', function(parte){
@@ -35,6 +49,10 @@ routes.post('/corpo',(req,res) => {
         res.json(JSON.parse(corpo))
     })
     */
+})
+//Isso é um body request com body parser
+routes.post('/corpo2',(req,res) => {
+    res.send(req.body)
 })
 
 //Isso aqui é um ROUTE PARAM
